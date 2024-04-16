1cd8b9628703471929983666a89da179472d7e9e
RodrigoMoreno2001 <rodrigoinsti06@gmail.com>
2024-04-16 19:09:58 +0200

    Anclas Smooth Scroll

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//este fichero hace smooth el scroll de las anclas
+function cuidados(){
+    document.getElementById("Cuidados").scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+}
+function introduccion(){
+    document.getElementById("Introduccion").scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+}
+function colores(){
+    document.getElementById("Colores").scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+}
+function curiosidades(){
+    document.getElementById("Curiosidades").scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+}
